e90e308cac40caf7ef05b09c35239e3769e7e4e0
EightBitBoy <haertel.dev@gmail.com>
2013-08-28 22:24:59 +0200

    get stuff up and running

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+#include <QApplication>
+#include <QTextStream>
+#include <QWidget>
 
-using namespace std;
+#include "mainwindow.h"
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int main(int argc, char *argv[]){
+	QApplication app(argc, argv);
+	QWidget window;
+
+	window.resize(250, 150);
+	window.setWindowTitle("Simple example");
+	window.show();
+
+
+	QTextStream out(stdout);
+	QString foo = "foobar";
+
+	out << foo << endl;
+
+    return app.exec();
 }
